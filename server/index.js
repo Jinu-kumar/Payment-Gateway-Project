a34c753a6503817b6a333ac3706eca3b9b1a47f3
Kumar-rishab3 <165635075+Kumar-rishab3@users.noreply.github.com>
2024-08-31 17:20:20 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const crypto = require("crypto");
+require("dotenv").config();
+
+const app = express();
+const PORT = "5000";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post("/order", async (req, res) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    const options = req.body;
+    const order = await razorpay.orders.create(options);
+
+    if (!order) {
+      return res.status(500).send("Error");
+    }
+
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
